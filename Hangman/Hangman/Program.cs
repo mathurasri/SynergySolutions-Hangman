409c15c5d34fdbdd686f80fc1dbcfd256f1cdccf
ayeshafawad <ayesha345@gmail.com>
2013-09-11 11:03:07 -0400

    hello hangman

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Hangman");
+            Console.WriteLine("Press <ENTER> to Exit.");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
